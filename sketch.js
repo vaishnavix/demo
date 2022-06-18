6a618ba11c4a7df087604eb12172cab85a9f9c8d
vaishnavix <107755052+vaishnavix@users.noreply.github.com>
2022-06-18 16:33:31 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+function setup() 
+{
+  createCanvas(400,400);
+  var weight=[20,50,70,60]
+  var sum= weight[0]+weight[1]+weight[2]+weight[3]
+  var average= sum/weight.length
+  console.log(average)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
